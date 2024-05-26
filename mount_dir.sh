336d790df2a04ec8f04cc58af7a206ecf6aeec3c
kasperr3000 <94169126+kasperr3000@users.noreply.github.com>
2024-05-26 11:25:04 +0200

    Update mount_dir.sh

--- mount_dir.sh
+++ mount_dir.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Define the sudo password
+PASSWORD="cheri"
+
 # Check if the directory exists, if not create it
 if [ ! -d "$HOME/cheri/DATA" ]; then
     echo "Creating directory ~/cheri/DATA"
@@ -10,7 +13,7 @@ fi
 
 # Mount the shared directory using 9p
 echo "Mounting the shared directory"
-sudo mount -t 9p -o trans=virtio,version=9p2000.L shared_dir "$HOME/cheri/DATA"
+echo "$PASSWORD" | sudo -S mount -t 9p -o trans=virtio,version=9p2000.L shared_dir "$HOME/cheri/DATA"
 
 # Check if the mount was successful
 if mountpoint -q "$HOME/cheri/DATA"; then
